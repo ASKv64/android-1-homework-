b787038d3f1ef9b901f4f3491b88d31184e6d571
Andrei <andrewskarzhinsky@gmail.com>
2021-06-17 01:27:30 +0300

    lesson1.2

--- lesson1/app/src/main/java/android/homework/lesson_1/MainActivity.java
+++ lesson1/app/src/main/java/android/homework/lesson_1/MainActivity.java
@@ -9,8 +9,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_ll);
-//        setContentView(R.layout.activity_cl);
+//        setContentView(R.layout.activity_ll);
+        setContentView(R.layout.activity_cl);
 //        setContentView(R.layout.activity_cv);
     }
 }
